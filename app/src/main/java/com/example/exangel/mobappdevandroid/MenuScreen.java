596c519794262713d4df895c0f10393e74c27059
exangel <tobiasdpetersen@gmail.com>
2014-09-07 20:23:05 +0200

    FirstCommit

---
+++ app/src/main/java/com/example/exangel/mobappdevandroid/MenuScreen.java
@@ -0,0 +1,81 @@
+package com.example.exangel.mobappdevandroid;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MenuScreen extends Activity implements OnClickListener {
+
+    Button settingsButton;
+    Button clickMeButton;
+    Button webButton;
+    Button vejledningButton;
+    TextView menuTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_screen);
+
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        clickMeButton = (Button) findViewById(R.id.clickMeButton);
+        webButton = (Button) findViewById(R.id.webButton);
+        vejledningButton = (Button) findViewById(R.id.vejledningButton);
+        vejledningButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(this);
+        clickMeButton.setOnClickListener(this);
+        webButton.setOnClickListener(this);
+
+        menuTextView = (TextView) findViewById(R.id.menuTextView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View v){
+        if(v == clickMeButton){
+            System.out.println("clickMeButton clicked!");
+            menuTextView.setText("Thank you!");
+            clickMeButton.setText("Click me again!");
+        } else if(v == webButton){
+            System.out.println("webButton clicked!");
+            Intent intent = new Intent(this, WebBrowserScreen.class);
+            startActivity(intent);
+        } else if(v == settingsButton) {
+            System.out.println("settingsButton clicked!");
+            Intent intent = new Intent(this, SettingsScreen.class);
+            startActivity(intent);
+        } else if(v == vejledningButton){
+            System.out.println("vejledningButton clicked!");
+            Intent intent = new Intent(this, vejledningScreen.class);
+            startActivity(intent);
+        } else {
+            System.out.println("Something unexpected clicked!");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/exangel/mobappdevandroid/WebBrowserScreen.java
@@ -0,0 +1,55 @@
+package com.example.exangel.mobappdevandroid;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class WebBrowserScreen extends Activity implements View.OnClickListener {
+    Button goButton;
+    EditText urlText;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_browser_menu);
+
+        goButton = (Button) findViewById(R.id.goButton);
+        goButton.setOnClickListener(this);
+        urlText = (EditText) findViewById(R.id.editText);
+        webView = (WebView) findViewById(R.id.browserWebView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.web_browser_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v){
+        String url = urlText.getText().toString();
+        //Uri uri = Uri.parse(url);
+        webView.loadUrl(url);
+    }
+}
